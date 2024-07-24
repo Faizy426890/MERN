ca4955a507ed1dec44eac8b145c89bec80417547
Faizy426890 <faizanlahori2003@gmail.com>
2024-07-24 06:18:43 +0500

    Describe the changes made

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserRoutes from './UserRoutes';
 import AdminRoutes from './AdminRoutes'; 
-import ConfirmDelivery from './ConfirmOrder';
+import ConfirmDelivery from './ConfirmOrder'; 
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        {/* Admin routes are accessible under /Login/* */}
         <Route path="/Login/*" element={<AdminRoutes />} />
+        
+        {/* Standalone route for ConfirmDelivery */}
+        <Route path="/confirmOrder" element={<ConfirmDelivery />} />
+
+        {/* User routes catch-all */}
         <Route path="*" element={<UserRoutes />} /> 
-    <Route path="/confirmOrder" element={<ConfirmDelivery />} />
       </Routes>
     </Router>
   );
