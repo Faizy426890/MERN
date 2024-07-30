b1f1303d1b5af29450dec5d950d409c60d48f11a
Faizy426890 <faizanlahori2003@gmail.com>
2024-07-30 23:04:05 +0500

    added config gile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'react', 
+        'react-dom',
+        'sweetalert2',  // Add SweetAlert2 if it's causing issues
+        'sweetalert2-react-content'  // Add SweetAlert2 React Content if it's causing issues
+      ],
+    },
+  },
 });
